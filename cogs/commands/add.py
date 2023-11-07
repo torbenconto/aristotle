1b661bfc118c9743a5d1cb62e29425e6edfec855
torbenconto <torben@2x1.dev>
2023-11-06 23:42:26 -0500

    add to mongo

--- cogs/commands/add.py
+++ cogs/commands/add.py
@@ -1,19 +1,37 @@
+import os
 import discord
 from discord.ext import commands, tasks
 from discord.commands import slash_command, Option
+import pymongo
+
+mongo_client = pymongo.MongoClient(str(os.environ.get('MONGODB_URI')))
 
 class Add(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.db = mongo_client["aristotle"]
+
     @slash_command(
         name='add',
         description='Adds a quote to the pool for your server.'
     )
     async def add(self, ctx, quote, author):
-        # add to pool
-        await ctx.respond(quote + " " + author)
-    
-    
+        # Get the server's ID and create a collection name based on it
+        server_id = str(ctx.guild.id)
+        collection_name = f"quotes_{server_id}"
+
+        # Access the collection and insert the document
+        collection = self.db[collection_name]
+        quote_document = {
+            "quote": quote,
+            "author": author
+        }
+        result = collection.insert_one(quote_document)
+
+        if result.acknowledged:
+            await ctx.respond("Quote added successfully.")
+        else:
+            await ctx.respond("Failed to add the quote.")
 
 def setup(bot):
     bot.add_cog(Add(bot))
