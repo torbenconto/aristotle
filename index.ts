604f072dc730dbb77c79913806b83c3aca260a6c
torbenconto <torben@2x1.dev>
2023-11-06 20:20:59 -0500

    start bot

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { Client } from "discord.js";
+
+console.log("Bot is starting...");
+
+const client = new Client({
+    intents: []
+});
+
+console.log(client); 
